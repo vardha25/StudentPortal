8195e9f2b02fad984d57a03c6ab543af45834cda
Amit Thakur <tamit9509@gmail.com>
2019-04-22 10:27:43 +0530

    - Add design for address details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressDetailsComponent } from './components/address-details/address-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressDetailsComponent
   ],
   imports: [
     BrowserModule,
