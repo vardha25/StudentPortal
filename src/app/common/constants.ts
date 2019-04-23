5f7fb073c259c3c79acfa8ba14963ae12af8b175
Amit Thakur <tamit9509@gmail.com>
2019-04-23 17:37:34 +0530

    - Add validation erroe messages in constant file.

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -3,3 +3,7 @@ export const AddressAPI = {
     States: 'https://raw.githubusercontent.com/tamit9509/Countries-States-Cities-database/master/states.json',
     Cities: 'https://raw.githubusercontent.com/tamit9509/Countries-States-Cities-database/master/cities.json'
 }
+export const Errors = {
+    RequiredField: 'This field is required!',
+    PinCode: 'Must be of 6 digits!'
+}
