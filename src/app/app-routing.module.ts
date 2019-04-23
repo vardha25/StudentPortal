74edd5b4f817033e890614a227e0d9243eca81ae
Amit Thakur <tamit9509@gmail.com>
2019-04-23 17:26:31 +0530

    - Add routing for address detail componentt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddressDetailsComponent } from './components/address-details/address-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'address-details', component: AddressDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
