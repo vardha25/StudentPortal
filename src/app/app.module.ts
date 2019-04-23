0dffe45b81d684536ba341fba075d549673231ab
vardha <vardha.jain@chicmic.co.in>
2019-04-23 12:34:13 +0530

    -Added photo-signature component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalDetailsComponent } from './components/personal-details/personal-details.component';
+import { PhotoSignatureComponent } from './components/photo-signature/photo-signature.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonalDetailsComponent
+    PersonalDetailsComponent,
+    PhotoSignatureComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/photo-signature/photo-signature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-signature',
+  templateUrl: './photo-signature.component.html',
+  styleUrls: ['./photo-signature.component.less']
+})
+export class PhotoSignatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
