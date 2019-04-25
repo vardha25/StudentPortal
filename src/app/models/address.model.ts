699553c98894f464b994c869ea3e73377338522c
Amit Thakur <tamit9509@gmail.com>
2019-04-25 16:23:39 +0530

    -  Add model for form

--- src/app/models/address.model.ts
+++ src/app/models/address.model.ts
@@ -12,11 +12,31 @@ interface CurrentAddress {
     city?: string;
     pincode: string;
 }
+interface PersonalDetail {
+    name: string,
+    fatherName: string,
+    motherName: string,
+    email: string,
+    dob: Date,
+    gender: string
+}
+
 export class AddressModel {
+    personalDetail: PersonalDetail;
     currentAddress: CurrentAddress;
     permanentAddress: PermanetAddress;
+
     constructor(currentAddress: CurrentAddress, permanentAddress: PermanetAddress) {
         this.currentAddress = currentAddress;
         this.permanentAddress = permanentAddress;
+    }
+    set personalDetailData(obj) {
+        this.personalDetail = obj;
+    }
+    set currentAddressData(obj) {
+        this.currentAddress = obj;
+    }
+    set permanentAddressData(obj) {
+        this.permanentAddress = obj
     }
 }
